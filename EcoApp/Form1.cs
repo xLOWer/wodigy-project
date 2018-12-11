f049794fa981a360e50e4350976d4925ec3d86be
Shishlo Dmitriy <venick007@gmail.com>
2018-12-12 01:36:50 +1000

    States changed

--- EcoApp/Form1.cs
+++ EcoApp/Form1.cs
@@ -7,48 +7,31 @@ namespace EcoApp
     // Класс формы Form1
     public partial class Form1 : Form
     {
-        /*
-         * В самом верху класса идёт конструктор(ы)
-         * Далее следуют свойства/поля/переменные в порядке:
-         *      1) private
-         *      2) protected
-         *      3) public
-         * Далее идут функции/методы/выражения в таком-же как свойства порядке
-         * 
-         * В файле HelpClass есть полезные подсказки
-         */
-
-        // Конструктор формы Form1
         public Form1()
         {
-            InitializeComponent(); // создаём кнопочки ИЛИ инициализируем компоненты формы
-            ops = new List<Operation>(); // инициализируем массив операций
-            TypeOperationCombobox.DataSource = Enum.GetValues(typeof(OperationType)); // задаём массив типов операций в ComboBox
-            StartView(); // выполняем функцию, блокирующую управление при старте
+            InitializeComponent();
+            ops = new List<Operation>();
+            TypeOperationCombobox.DataSource = Enum.GetValues(typeof(OperationType));
+            SetNonState();
         }
 
-        List<Operation> ops { get; set; } // свойство, массив операций
-        OperationType? OperationCurrentType { get; set; } // тут хранится выбранный тип операции
-        decimal OperationCurrentValue { get; set; } // тут хранится выбранное значение операции
-        string OperationCurrentName { get; set; } // тут хранится выбранное имя операции
-        TreeNode SelectedNode { get; set; } // тут хранится выбранное имя операции
-
-
-        // Функция отвечающая за клик по кнопке "Добавить"
+        List<Operation> ops { get; set; } 
+        OperationType? OperationCurrentType { get; set; } 
+        decimal OperationCurrentValue { get; set; }
+        string OperationCurrentName { get; set; }
+        TreeNode SelectedNode { get; set; }
+                
         private void AddLevelButton_Click(object sender, EventArgs e)
         {
-            if (SelectedNode == null) return; // проверим, а не null ли наша выбранная нода(в см. выбрали ли мы что-то)
-
-            TypeOperationCombobox.Enabled = true;
-            NameOperationBox.Enabled = true;
-            ValueOperationBox.Enabled = true;
-            CancelButton.Enabled = true;
-
+            if (SelectedNode == null) return;
         }
 
-        // Функция отвечающая за блокировку элементов управления при старте
-        public void StartView()
+        //Set - Установить
+        //Non - Не/Отсутствие
+        //State - Состояние
+        public void SetNonState() 
         {
+            // устанавливаем флаги доступа
             AddLevelButton.Enabled = false;
             EditSelectedButton.Enabled = false;
             DeleteSelectedButton.Enabled = false;
@@ -57,24 +40,23 @@ namespace EcoApp
             ValueOperationBox.Enabled = false;
             SaveButton.Enabled = false;
             CancelButton.Enabled = false;
-        }
 
-        // Функция отвечающая за клик по кнопке "Отмена"
-        private void CancelButton_Click(object sender, EventArgs e)
-        {
-            StartView(); // задаём изначальный вид
-            OperationCurrentType = null; // обнуляем свойства
+            // сбрасываем свойства
+            OperationCurrentType = null;
             OperationCurrentName = null;
             OperationCurrentValue = 0;
         }
-
-        // Функция отвечающая за изменение текста в текстовом поле NameOperation
+        
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            SetNonState();
+        }
+        
         private void NameOperationBox_TextChanged(object sender, EventArgs e)
         {
 
         }
-
-        // Функция отвечающая за клик по кнопке "Сохранить"
+        
         private void SaveButton_Click(object sender, EventArgs e)
         {
             Operation newOp = new Operation()
@@ -85,47 +67,37 @@ namespace EcoApp
             };
             ops.Add(newOp);
 
-            SelectedNode.Nodes.Add(new TreeNode(newOp.Name+" | "+ newOp.Type + " | " + newOp.Value));
+            SelectedNode.Nodes.Add(new TreeNode(newOp.Name + " | " + newOp.Type + " | " + newOp.Value));
             SelectedNode.Expand();
-            OperationCurrentType = null; // обнуляем свойства
-            OperationCurrentName = null;
-            OperationCurrentValue = 0;
-            StartView(); // задаём изначальный вид
+            SetNonState();
         }
-
-        // Функция отвечающая за клик по кнопке "Удалить"
+        
         private void DeleteSelectedButton_Click(object sender, EventArgs e)
         {
 
         }
-
-        // Функция отвечающая за клик по кнопке "Редактировать"
+        
         private void EditSelectedButton_Click(object sender, EventArgs e)
         {
             TypeOperationCombobox.SelectedValue = OperationCurrentType;
-            NameOperationBox.Text = OperationCurrentName; // задаём свойства
+            NameOperationBox.Text = OperationCurrentName;
             ValueOperationBox.Value = OperationCurrentValue;
         }
-
-        // Функция срабатывающая в момент изменения текста в TypeBox
+        
         private void TypeOperationCombobox_SelectedValueChanged(object sender, EventArgs e)
         {
             OperationPropertiesValidation();
         }
         
-        // Функция проверяет всё ли впорядке в полях ввода и возвращает true если всё ок
         private void OperationPropertiesValidation()
         {
-            // "классический" подход
-            if (String.IsNullOrEmpty(OperationCurrentName) // проверяем, выбрано ли чтото в TypeBox
-                && OperationCurrentType is OperationType) // заполнено ли поле NameBox
+            if (String.IsNullOrEmpty(OperationCurrentName) && OperationCurrentType.HasValue)
             {
                 SaveButton.Enabled = true;
             }
             SaveButton.Enabled = false;
         }
-
-        // Функция срабатывающая в момент изменения текста в ValueBox
+        
         private void ValueOperationBox_ValueChanged(object sender, EventArgs e)
         {
             OperationCurrentValue = ValueOperationBox.Value;
@@ -136,13 +108,11 @@ namespace EcoApp
         {
             if (treeView1.SelectedNode == null) return;
             SelectedNode = treeView1.SelectedNode;
-            AddLevelButton.Enabled = true;
-            DeleteSelectedButton.Enabled = true;
-            EditSelectedButton.Enabled = true;
             OperationCurrentType = (OperationType)TypeOperationCombobox.SelectedValue;
-            OperationCurrentName = NameOperationBox.Text; // задаём свойства
+            OperationCurrentName = NameOperationBox.Text;
             OperationCurrentValue = ValueOperationBox.Value;
             OperationPropertiesValidation();
         }
+
     }// public partial class Form1 : Form
 } // namespace EcoApp
